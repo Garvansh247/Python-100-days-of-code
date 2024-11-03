1a0bb165396110ffb9ebc37d293354a3736b3dc7
Garvansh247 <garvansh247@gmail.com>
2024-11-03 19:44:29 +0530

    update main.py

--- 09-Day9-Typecasting-in-Python/main.py
+++ 09-Day9-Typecasting-in-Python/main.py
@@ -1,6 +1,6 @@
 a = "1"
 # a = 1
-b = "2" # direct addition of a and b eithout typecasting will lead to concatenation of the two strings a + b = "12"
+b = "2" # direct addition of a and b without typecasting will lead to concatenation of the two strings a + b = "12"
 # b = 2
 print(int(a) + int(b))
 
