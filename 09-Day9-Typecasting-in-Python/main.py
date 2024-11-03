16a30c6ffe7d8843ac43d406a23cce230c182d09
Garvansh247 <garvansh247@gmail.com>
2024-11-03 22:41:40 +0530

    str() function included

--- 09-Day9-Typecasting-in-Python/main.py
+++ 09-Day9-Typecasting-in-Python/main.py
@@ -3,9 +3,12 @@ a = "1"
 b = "2" # direct addition of a and b without typecasting will lead to concatenation of the two strings a + b = "12"
 # b = 2
 print(int(a) + int(b))
+c=1
+d=2
+print(str(c)+str(d))
 
 # Implicit TypeCasting
-c = 1.9
-d = 8
+e = 1.9
+f = 8
 
-print(c + d)
+print(e + f)
